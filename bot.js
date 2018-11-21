9821cba4c314cd0ac3a232899c021c37bf9e5969
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2018-11-21 11:53:31 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('======================================')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('=======================================')
+});
+
+
+
+
+
+const prefix = ['.']
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "say" + "<@" + `${client.user.id}` + ">") {
+let rank = message.guild.member(message.author).roles.find('name', '.');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+client.login(process.env.BOT_TOKEN); 
