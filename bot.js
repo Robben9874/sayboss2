725cec7a3d0e1f7a3264a32a576b77ab2e25e5fd
Robben9874 <37851177+Robben9874@users.noreply.github.com>
2020-02-10 21:12:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const initcmd = '#';
 
 client.on('ready', () => {
   console.log('======================================')
@@ -14,26 +13,8 @@ client.on('ready', () => {
 
 
 
-
-
-
-client5.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(initcmd)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(initcmd.length);
-
-  let args = message.content.split(" ").slice(1);
-
-
-  
-if (command == "f5") {
-let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
+client.on('message', message => {
+    if(message.content === '-credit'){
+        message.channel.send('#credit')
+    }
 });
-
-client.login(process.env.BOT_TOKEN); 
